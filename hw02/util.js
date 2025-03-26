025ac8852a5890a5bfc4c431ae5fd3b53e7e0bf1
hyerin26 <hrnnn@yonsei.ac.kr>
2025-03-26 15:21:59 +0900

    Update util.js

--- hw02/util.js
+++ hw02/util.js
@@ -1,22 +1,13 @@
 // resizeAspectRatio() 함수 구현
-
-export function resizeAspectRatio(gl, canvas) {
-    window.addEventListener('resize', () => {
-        // Calculate new canvas dimensions while maintaining aspect ratio
-        const originalWidth = canvas.width;
-        const originalHeight = canvas.height;
-        const aspectRatio = originalWidth / originalHeight;
-        let newWidth = window.innerWidth;
-        let newHeight = window.innerHeight;
-
-        if (newWidth / newHeight > aspectRatio) {
-            newWidth = newHeight * aspectRatio;
-        } else {
-            newHeight = newWidth / aspectRatio;
-        }
-
-        canvas.width = newWidth;
-        canvas.height = newHeight;
-        gl.viewport(0, 0, canvas.width, canvas.height);
-    });
+export function resizeAspectRatio(canvas, gl) {
+    let size;
+    if (window.innerWidth < window.innerHeight) { 
+        size = window.innerWidth;
+    }
+    else {
+        size = window.innerHeight;
+    }
+    canvas.width = size;
+    canvas.height = size;
+    gl.viewport(0, 0, size, size);
 }
